3036ed5336ae7ca472865c43b24e7da94a93e0f6
Yangismee _ <giang.nt09845@sinhvien.hoasen.edu.vn>
2025-11-18 16:20:50 +0700

    deploy v2.8

--- my-medusa-store/vite.config.js
+++ my-medusa-store/vite.config.js
@@ -4,7 +4,7 @@ export default defineConfig({
   server: {
     allowedHosts: [
       "ecommerceproject-production-679b.up.railway.app",
-      ".railway.app" // Cho phép tất cả subdomain của railway nếu cần
+      ".railway.app"
     ]
   }
 })
