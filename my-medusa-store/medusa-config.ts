42b9aea380b2a4a566ba5807f3e233c6cbe4c10e
Yangismee _ <giang.nt09845@sinhvien.hoasen.edu.vn>
2025-12-02 11:37:39 +0700

    Push code fe, feture load  Product

--- my-medusa-store/medusa-config.ts
+++ my-medusa-store/medusa-config.ts
@@ -1,7 +1,5 @@
 import { loadEnv, defineConfig } from "@medusajs/framework/utils";
 
-
-
 loadEnv(process.env.NODE_ENV || "development", process.cwd());
 console.log("✅ AUTH_CORS:", process.env.AUTH_CORS);
 console.log("✅ STORE_CORS:", process.env.STORE_CORS);
@@ -9,8 +7,6 @@ console.log("✅ STORE_CORS:", process.env.STORE_CORS);
 module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
-    redisUrl: process.env.REDIS_URL,
-    workerMode: (process.env.WORKER_MODE as "shared" | "worker" | "server") || "shared",
     http: {
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
@@ -19,41 +15,21 @@ module.exports = defineConfig({
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
     },
     databaseDriverOptions: {
-      connection: {
-        ssl: {
-          rejectUnauthorized: false, // Chấp nhận chứng chỉ SSL của Neon
-        },
-      },
+      ssl: false,
+      sslmode: "disable",
     },
   },
-  // modules: [
-  //   // ...
-  //   {
-  //     resolve: "@medusajs/medusa/auth",
-  //     dependencies: [Modules.CACHE, ContainerRegistrationKeys.LOGGER],
-  //     options: {
-  //       providers: [
-  //         // other providers...
-  //         {
-  //           resolve: "@medusajs/medusa/auth-emailpass",
-  //           id: "emailpass",
-  //         },
-  //         {
-  //           resolve: "@medusajs/medusa/auth-google",
-  //           id: "google",
-  //           options: {
-  //             clientId: process.env.GOOGLE_CLIENT_ID,
-  //             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  //             callbackUrl:
-  //               process.env.GOOGLE_CALLBACK_URL ||
-  //               "http://localhost:3000/auth/callback",
-  //             successRedirect: "http://localhost:3000/auth/callback", // hoặc "/" rồi xử lý token nếu backend appends token
-  //             failureRedirect:
-  //               "http://localhost:3000/login?error=google_login_failed",
-  //           },
-  //         },
-  //       ],
-  //     },
-  //   },
-  // ],
+  admin: {
+    // Hàm này giúp cấu hình lại Vite đang chạy ngầm bên trong
+    vite: (config) => {
+      return {
+        ...config,
+        server: {
+          ...config.server,
+          // Cho phép tất cả các host (Bao gồm cả link Cloudflare)
+          allowedHosts: true, 
+        },
+      };
+    },
+  },
 });
